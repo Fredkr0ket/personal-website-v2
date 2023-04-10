4ebb208075144589bf0ed3c3f4cc05a9ac8c9748
Fredkr0ket <ferdovbalen@gmail.com>
2023-04-10 09:50:20 +0200

    added some info and removed some info

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,5 +1,4 @@
 export const info = {
-  baseUrl: "https://astro-portfolio-uzair.vercel.app",
   name: "Ferdo van Balen",
   jobDescription: "Software Developer",
   about: `
@@ -13,52 +12,40 @@ export const info = {
       location: "Leeuwarden",
       startDate: "Sep 2022",
       endDate: "Feb 2023",
-      description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
-      ],
+      description: [],
     },
   ],
 
   education: [
     {
-      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2019",
-      endDate: "2022",
-      description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
-      ],
+      name: "Software Developer Niveau 4 - ROC Friesepoort",
+      location: "Drachten",
+      startDate: "2021",
+      endDate: "Heden",
+      description: [],
     },
     {
-      name: "Govt. Degree Boys College Buffer Zone",
-      location: "Karāchi, Sindh, Pakistan",
+      name: "MAVO - CSG Liudger",
+      location: "Drachten",
+      startDate: "2020",
+      endDate: "2021",
+      description: [],
+    },
+    {
+      name: "HAVO - CSG Liudger",
+      location: "Drachten",
       startDate: "2018",
       endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
-    },
-    {
-      name: "Ghazi Foundation Schools & Colleges",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2016",
-      endDate: "2018",
-      description: ["Matriculation,  Computer Science"],
+      description: [],
     },
   ],
 
   socialMedia: {
     github: "https://github.com/Fredkr0ket",
-    email: "uzairahmed@74372gmail.com",
+    email: "ferdovbalen@gmail.com",
     linkedin: "https://www.linkedin.com/in/ferdo-van-balen-441879241/",
   },
 
   projects: [
-    {
-      title: "Astro Portfolio",
-      isFeatured: true,
-      thumbnail: "/assets/images/astro-portfolio.png",
-      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
-      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
-    },
   ],
 };
